79992ed84d6f429e28c61badd5ede6575d75142f
Maksim Kostromin <daggerok@gmail.com>
2018-06-15 01:52:54 +0300

    --help (C) whatthecommit.com

---
+++ src/main/java/daggerok/app/ExchangeProcessorCamelConfig.java
@@ -0,0 +1,35 @@
+package daggerok.app;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.camel.Message;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
+import static java.lang.String.format;
+
+//tag::process-exchange[]
+@Configuration
+@RequiredArgsConstructor
+public class ExchangeProcessorCamelConfig {
+
+  @Bean
+  public RouteBuilder processExchangeRoute() {
+    return new RouteBuilder() {
+      @Override
+      public void configure() throws Exception {
+        from("file:///tmp/camel-exchange-in")
+            .routeId("exchange-processor")
+            .process().exchange(exchange -> {
+              final Message message = exchange.getMessage();
+              final Object body = message.getBody();
+              log.info("handling message body in exchange consumer: {}", body);
+            })
+            .to("file:///tmp/camel-exchange-out");
+      }
+    };
+  }
+}
+//end::process-exchange[]
