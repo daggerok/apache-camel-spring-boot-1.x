d0f2baa3cfad77150358bce3a0c0e62af72922d4
Maksim Kostromin <daggerok@gmail.com>
2018-06-14 22:10:30 +0300

    this doesn't really make things faster, but I tried (C) whatthecommit.com

--- src/main/java/daggerok/app/MoveFilesCamelConfig.java
+++ src/main/java/daggerok/app/MoveFilesCamelConfig.java
@@ -1,35 +1,24 @@
 package daggerok.app;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-
+//tag::content[]
 @Configuration
 @RequiredArgsConstructor
 public class MoveFilesCamelConfig {
-
-  final CamelContext camelContext;
-  //final ConnectionFactory connectionFactory;
-
-  @PostConstruct
-  public void initCamelJmsComponent() {
-    //final JmsComponent jmsComponent = JmsComponent.jmsComponent(connectionFactory);
-    //this.camelContext.addComponent("my-jms-component", jmsComponent);
-  }
 
   @Bean
   public RouteBuilder copyDirectoryRoute() {
     return new RouteBuilder() {
       @Override
       public void configure() throws Exception {
-        //from("my-jms-component:queue:my-queue")
         from("file:///tmp/camel-in")
             .to("file:///tmp/camel-out");
       }
     };
   }
 }
+//end::content[]
